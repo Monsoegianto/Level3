08d4a7f9ffae018a801a4234a4dbeef5df280032
tsoegianto <MonSoegianto@monikasantosair.wifi.bcit.ca>
2018-10-11 13:28:52 -0700

    edit

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ function RandomRed(){
   return "rgb("+red+", 100, 100)";
 }
 
+var i = 0;
+setInterval(()=>{
+	if(i%2==0){	
+	app.style.backgroundColor = RandomRed();
+       }else {
+	app.style.backgroundColor = RandomBlue();
+       }
+       i++;
+     }, 500);
+
 setInterval(()=>{
 	app.style.backgroundColor = "rgb(255,222,255)";
  },500);
